0b8f521d2b9eb8ffb733759a27886565a9098061
Jeffrey Guan <jeffreyg2240@hstat.org>
2019-03-18 03:54:55 +0000

    finished

--- application_controller.rb
+++ application_controller.rb
@@ -15,15 +15,22 @@ class ApplicationController < Sinatra::Base
   
   post '/result' do
     begin
-        puts @steamID = params[:steam_id]
+        @steamID = params[:steam_id]
+        #[user name, steam id, steam URL, steam avatar]
         @steam_data = user(@steamID)
         @steamID = @steam_data[1].to_i
+        #[total hours, hours within 2 weeks, Number of games]
         @steam_time = hoursplayed(@steamID)
+        #[{game = total time played}, {game = total time played}.....]
         @games = steam_games(@steamID)
+        #[community ban, VAC ban, game ban, trade ban]
         @steam_bans = bans(@steamID)
-        puts @steamID
       rescue
         @steamID = "Invalid ID"
+        @steam_data = ["???","???", "???", "https://steamuserimages-a.akamaihd.net/ugc/885384897182110030/F095539864AC9E94AE5236E04C8CA7C2725BCEFF/"]
+        @steam_bans = ["N/A","N/A","N/A","N/A"]
+        @steam_time = ["???","???", "???"]
+        @games = ["???" => "???"]
       end
       erb :result
     end
--- models/model.rb
+++ models/model.rb
@@ -33,32 +33,28 @@ steam_games_id_converter = JSON.parse(Net::HTTP.get(URI("https://api.steampowere
         games = []
         game_time = []
         pairing = []
-        begin
-            Steam::Player.owned_games(steam_id)['games'].each{ |game_specs|
-               game_time << game_specs['playtime_forever'].to_i/60
-            }
-            total_games = Steam::Player.owned_games(steam_id)['games']
-                #Total number of games the account owns
-                
-                #Tterates through each hash(game)
-                total_games.each { |x|
-                    #2nd API which is used to turn the game ID into itle
-                    #'game' is a array of hashes of all of steam's games
-                    game = JSON.parse(Net::HTTP.get(URI("https://api.steampowered.com/ISteamApps/GetAppList/v2/")))["applist"]["apps"].select { |app_id| 
-                        # puts app_id['appid']
-                        if app_id['appid'] == x['appid']
-                            games << app_id['name']
-                            break
-                        end
-                    }
+        Steam::Player.owned_games(steam_id)['games'].each{ |game_specs|
+           game_time << game_specs['playtime_forever'].to_i/60
+        }
+        total_games = Steam::Player.owned_games(steam_id)['games']
+            #Total number of games the account owns
+            #Tterates through each hash(game)
+            total_games.each { |x|
+                #2nd API which is used to turn the game ID into itle
+                #'game' is a array of hashes of all of steam's games
+                game = JSON.parse(Net::HTTP.get(URI("https://api.steampowered.com/ISteamApps/GetAppList/v2/")))["applist"]["apps"].select { |app_id| 
+                    # puts app_id['appid']
+                    if app_id['appid'] == x['appid']
+                        games << app_id['name']
+                        break
+                    end
                 }
-                for i in 0...games.length do
-                    pairing << {games[i] => game_time[i]} 
-                end
-                 pairing
+            }
+            for i in 0...games.length do
+                pairing << {games[i] => game_time[i]} 
             end
+        pairing
     end
-    
     def bans(steam_id)
         sBans = []
         #Community Ban
@@ -85,6 +81,7 @@ steam_games_id_converter = JSON.parse(Net::HTTP.get(URI("https://api.steampowere
         else
             sBans << "Yes"
         end
+        sBans
     end
     
     def user(steam_id)
@@ -111,13 +108,11 @@ steam_games_id_converter = JSON.parse(Net::HTTP.get(URI("https://api.steampowere
         twoweek = 0
         hours = 0
         begin
-            count = Steam::Player.owned_games(steam_id)['game_count']
-            total_games = Steam::Player.owned_games(steam_id)
-            game_list = Steam::Player.owned_games(steam_id)['games']    
+            total_games = Steam::Player.owned_games(steam_id)["game_count"]
             # #Goes through every game and adds up all the total minutes played in the account
             Steam::Player.owned_games(steam_id)['games'].find_all{ |minutes_played| hours += minutes_played["playtime_forever"]}
             # #Divides minutes to get hours
-             hours /= 60
+            hours /= 60
             # #Goes through every game and adds up the total number of minutes played within the past 2 weeks
             Steam::Player.owned_games(steam_id)['games'].find_all{ |minutes_played| twoweek += minutes_played["playtime_2weeks"].to_i}#The "playtime_2weeks" was not a FixNum
             # #Divides minutes to get hours
@@ -125,19 +120,16 @@ steam_games_id_converter = JSON.parse(Net::HTTP.get(URI("https://api.steampowere
         rescue
             hours = "Private Profile"
             twoweek = "Private Profile"
+            total_games = "Private Profile"
         end
-        time = [hours, twoweek, count]
+        time = [hours, twoweek, total_games]
+        time
     end
 
 # https://www.rubydoc.info/gems/steam-api/Steam/Player
 
-
+#   <% @games.each do |game| %>
+#              <%= game %>
+#           <% end %>
     #Screenshot for example
     # https://steamcommunity.com/id/questionablegoblin
-    #     @steamID = gets.chomp
-    #   puts @steam_data = user(@steamID)
-    #     puts   @steamID = @steam_data[1].to_i
-    #     puts @steam_time = hoursplayed(@steamID)
-    #     puts @games = steam_games(@steamID)
-    #     puts @steam_bans = bans(@steamID)
-    #     # puts @steamID
